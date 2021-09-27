2f4a084a18b7cf690d6d9f2d403b7794c390df50
vothang251199 <taoa251199@gmail.com>
2021-09-27 20:39:41 +0700

    Add Entity

---
+++ Testcore5.Data/EF/Testcore5DbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcore5.Data.Entities;
+
+namespace Testcore5.Data.EF
+{
+    public class Testcore5DbContext : DbContext
+    {
+        public Testcore5DbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppConfig> AppConfigs { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails{ get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductInCategory> ProductInCategories { get; set; }
+        public DbSet<ProductTranslation> ProductTranslations { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
