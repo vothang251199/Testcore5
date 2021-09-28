5791316abe141cbd15281da0bb4fcddca4017ddb
vothang251199 <taoa251199@gmail.com>
2021-09-28 09:06:34 +0700

    Create database first

---
+++ Testcore5.Data/Configurations/AppConfigConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcore5.Data.Entities;
+
+namespace Testcore5.Data.Configurations
+{
+    public class AppConfigConfiguration : IEntityTypeConfiguration<AppConfig>
+    {
+        public void Configure(EntityTypeBuilder<AppConfig> builder)
+        {
+            builder.ToTable("AppConfigs");
+
+            builder.HasKey(x => x.Key);
+
+            builder.Property(x => x.Value).IsRequired(true);
+        }
+    }
+}
---
+++ Testcore5.Data/EF/TestC5DbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testcore5.Data.EF
+{
+    public class TestC5DbContextFactory : IDesignTimeDbContextFactory<TestC5DbContext>
+    {
+        public TestC5DbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("Testcore5Db");
+
+            var optionsBuilder = new DbContextOptionsBuilder<TestC5DbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new TestC5DbContext(optionsBuilder.Options);
+        }
+    }
+}
